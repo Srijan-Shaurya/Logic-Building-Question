be557b6113c98dec730e85776e73c49e7d9b94a5
Srijan Shaurya <i.srijan.shaurya@gmail.com>
2026-05-30 07:06:13 +0530

    CT-L1-Q1

--- src/first.java
+++ src/first.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
-
+//Take a number and print whether it’s positive, negative, or zero.
 public class first {
     static void main() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Hello");
+        System.out.println("This Program Will Help You To Find Out Whether The Number is Positive, Negative Or Zero");
+        System.out.print("Enter The Number: ");
+        int num = input.nextInt();
+        if(num>0){
+            System.out.println("Positive");
+        }
+        else if(num<0){
+            System.out.println("Negative");
+        } else{
+            System.out.println("Zero");
+        }
     }
 }
