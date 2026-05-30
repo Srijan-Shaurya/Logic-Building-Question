8a23aa02f3cd7e9af9128bade2a6b8b4e0be25f7
Srijan Shaurya <i.srijan.shaurya@gmail.com>
2026-05-30 08:18:55 +0530

    Divisibility Checker for 5

---
+++ src/Third.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+//Check if a number is divisible by 5.
+public class Third{
+    static void main() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome! to Divisibility Checker of 5");
+        System.out.println("Enter The Number: ");
+        int n = input.nextInt();
+//        Here I have used ternary Conditional Operator
+        String Status = n%5==0 ? "Divisible by 5" : "Not divisible by 5";
+        System.out.println(Status);
+    }
+}
